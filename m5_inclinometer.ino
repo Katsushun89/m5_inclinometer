bd5f1fca5195ce54347103c9aef6e1b9698cb62e
Katsushun89 <Katsushun89@gmail.com>
2021-09-01 00:49:15 +0900

    Implement Kalman filter but there are some bugs.

--- m5_inclinometer.ino
+++ m5_inclinometer.ino
@@ -1,4 +1,10 @@
+#include <M5Stack.h>
+#include <Ticker.h>
 
+#include "imu.h"
+#include "utility/MPU9250.h"
+
+MPU9250 IMU;
 
 // Ticker
 Ticker timer1;
@@ -70,7 +76,7 @@ void mat_mul(float *m1, float *m2, float *sol, int row1, int column1, int row2,
 }
 
 // Matrix transposition
-void mat_trans(float *m1, float *sol, int row_original, int column_original) {
+void mat_tran(float *m1, float *sol, int row_original, int column_original) {
     for (int i = 0; i < row_original; i++) {
         for (int j = 0; j < column_original; j++) {
             sol[j * row_original + i] = m1[i * column_original + j];
@@ -79,7 +85,7 @@ void mat_trans(float *m1, float *sol, int row_original, int column_original) {
 }
 
 // Matrix scalar maltiplication
-void mat_trans(float *m1, float c, float *sol, int row, int column) {
+void mat_mul_const(float *m1, float c, float *sol, int row, int column) {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++) {
             int index = i * column + j;
@@ -89,7 +95,7 @@ void mat_trans(float *m1, float c, float *sol, int row, int column) {
 }
 
 // Matrix inversion ( by Gaussian elimination)
-void mat_trans(float *m, float *sol, int row, int column) {
+void mat_inv(float *m, float *sol, int row, int column) {
     // alloc
     float *temp = (float *)malloc(column * 2 * row * sizeof(float));
 
@@ -164,4 +170,158 @@ void mat_trans(float *m, float *sol, int row, int column) {
         }
     }
     free(temp);
+}
+
+float get_acc_data() {
+    float theta_deg = atan(float(IMU.az) / float(IMU.ay));
+    return theta_deg * 57.2958;
+}
+
+float get_gyro_data() { return IMU.gx * IMU.gRes; }
+
+void acc_init() {
+    float theta_array[sample_num];
+    for (int i = 0; i < sample_num; i++) {
+        calcIMU(&IMU);
+        theta_array[i] = get_acc_data();
+        delay(meas_interval);
+    }
+
+    // calculate mean
+    theta_mean = 0;
+    for (int i = 0; i < sample_num; i++) {
+        theta_mean += theta_array[i];
+    }
+    theta_mean /= sample_num;
+
+    float temp;
+    theta_variance = 0;
+    for (int i = 0; i < sample_num; i++) {
+        temp = theta_array[i] - theta_mean;
+        theta_variance += temp * temp;
+    }
+    theta_variance /= sample_num;
+}
+
+void gyro_init() {
+    float theta_dot_array[sample_num];
+    for (int i = 0; i < sample_num; i++) {
+        calcIMU(&IMU);
+        theta_dot_array[i] = get_gyro_data();
+        delay(meas_interval);
+    }
+
+    // calculate mean
+    theta_dot_mean = 0;
+    for (int i = 0; i < sample_num; i++) {
+        theta_dot_mean += theta_dot_array[i];
+    }
+    theta_dot_mean /= sample_num;
+
+    float temp;
+    theta_dot_variance = 0;
+    for (int i = 0; i < sample_num; i++) {
+        temp = theta_dot_array[i] - theta_dot_mean;
+        theta_dot_variance += temp * temp;
+    }
+    theta_dot_variance /= sample_num;
+}
+
+// Kalman filter for theta and theta_dot_bias
+void update_theta() {
+    calcIMU(&IMU);
+    // measurement data
+    float y = get_acc_data();
+
+    // input data
+    float theta_dot_gyro = get_gyro_data();
+
+    // calculate Kalman gain: G = P`C^T(W+CP`C^T)^-1
+    float P_CT[2][1] = {};
+    float tran_C_theta[2][1] = {};
+    mat_tran(C_theta[0], tran_C_theta[0], 1, 2);                        // C^T
+    mat_mul(P_theta_predict[0], tran_C_theta[0], P_CT[0], 2, 2, 2, 1);  // P`C^T
+
+    float G_temp1[1][1] = {};
+    mat_mul(C_theta[0], P_CT[0], G_temp1[0], 1, 2, 2, 1);  // CP`C^T
+
+    float G_temp2 = 1.0f / (G_temp1[0][0] + theta_variance);  //(W+CP`C^T)^-1
+    float G[2][1] = {};
+    mat_mul_const(P_CT[0], G_temp2, G[0], 2, 1);  // P`C^T(W+CP`C^T)^-1
+
+    // theta_data estimation: theta = theta` + G(y - Ctheta`)
+    float C_theta_theta[1][1] = {};
+    mat_mul(C_theta[0], theta_data_predict[0], C_theta_theta[0], 1, 2, 2,
+            1);                               // Ctheta`
+    float delta_y = y - C_theta_theta[0][0];  // y - Ctheta`
+    float delta_theta[2][1] = {};
+    mat_mul_const(G[0], delta_y, delta_theta[0], 2, 1);
+    mat_add(theta_data_predict[0], delta_theta[0], theta_data[0], 2, 1);
+
+    // calculate covariance matrix: P=(I-GC)P`
+    float GC[2][2] = {};
+    float I2[2][2] = {{1, 0}, {0, 1}};
+    mat_mul(G[0], C_theta[0], GC[0], 2, 1, 1, 2);  // GC
+    float I2_GC[2][2] = {};
+    mat_sub(I2[0], GC[0], I2_GC[0], 2, 2);                          // I-GC
+    mat_mul(I2_GC[0], P_theta_predict[0], P_theta[0], 2, 2, 2, 2);  //((I-GC)P`
+
+    // predict the next step data: theta`
+    float A_theta_theta[2][1] = {};
+    float B_theta_dot[2][1] = {};
+    mat_mul(A_theta[0], theta_data[0], A_theta_theta[0], 2, 2, 2, 1);  // Atheta
+    mat_mul_const(B_theta[0], theta_dot_gyro, B_theta_dot[0], 2, 1);   // Bu
+    mat_add(A_theta_theta[0], B_theta_dot[0], theta_data_predict[0], 2,
+            1);  // Atheta+Bu
+
+    // predict covariance matrix: P`=APA^T + BUB^T
+    float AP[2][2] = {};
+    float APAT[2][2] = {};
+    float tran_A_theta[2][2] = {};
+    mat_tran(A_theta[0], tran_A_theta[0], 2, 2);           // A^T
+    mat_mul(A_theta[0], P_theta[0], AP[0], 2, 2, 2, 2);    // AP
+    mat_mul(AP[0], tran_A_theta[0], APAT[0], 2, 2, 2, 2);  // APA^T
+
+    float BBT[2][2] = {};
+    float tran_B_theta[1][2] = {};
+    mat_tran(B_theta[0], tran_B_theta[0], 2, 1);               // B^T
+    mat_mul(B_theta[0], tran_B_theta[0], BBT[0], 2, 1, 1, 2);  // BB^T
+
+    float BUBT[2][2] = {};
+    mat_mul_const(BBT[0], theta_dot_variance, BUBT[0], 2, 2);  // BB^T
+    mat_add(APAT[0], BUBT[0], P_theta_predict[0], 2, 2);       // APA^T+BUB^T
+}
+
+void setup() {
+    Serial.begin(115200);
+    // Power ON Stabilizing...
+    delay(500);
+    M5.begin();
+    Wire.begin();
+
+    initIMU(&IMU);
+
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setTextColor(GREEN);
+    M5.Lcd.setTextSize(2);
+
+    // Accelemeter and gyro calibration
+    acc_init();
+    gyro_init();
+
+    // Kalman filter initialization
+    theta_data_predict[0][0] = 0;
+    theta_data_predict[1][0] = theta_dot_mean;
+
+    P_theta_predict[0][0] = 1;
+    P_theta_predict[0][1] = 0;
+    P_theta_predict[1][0] = 0;
+    P_theta_predict[1][1] = theta_dot_variance;
+
+    timer1.attach(theta_update_interval, &update_theta);
+}
+
+void loop() {
+    Serial.printf("theta=%f deg\n", theta_data[0][0]);
+    delay(0.05);
 }
